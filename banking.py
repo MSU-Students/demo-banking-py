5bfc5a28974d84271ea2e24e1892e58b5bd51221
fencim <fencim@gmail.com>
2025-03-13 13:56:42 +0800

    use class based instead of procedures

--- banking.py
+++ banking.py
@@ -4,13 +4,53 @@ TERMINATE, WITHDRAW, DEPOSIT, BALANCE, SUMMARY, WITHDRAW_SUMMARY = (0, 1 , 2, 3,
 class BankAccount:
     def __init__(self, accountName):
         self.name = accountName
-        
+
     accountBalance = 0
     transactions = []
 
-# banking storage 
-accountBalance = 0
-transactions = []
+    # manage deposit operation
+    def depositAmount(self):
+        deposit = float(input('Enter amount to deposit:'))
+        if deposit <= 0:
+            print('Invalid amount')
+        else:
+            self.accountBalance += deposit
+            self.transactions.append(('deposit', deposit))
+    
+    # manage withdrawal operation
+    def withdrawAmount(self):
+        withdraw = float(input('Enter amount to withdraw:'))
+        if withdraw > self.accountBalance:
+            print('Invalid amount')
+        else:
+            self.accountBalance -= withdraw
+            self.transactions.append(('withdraw', withdraw))
+
+    # Display present balance
+    def checkBalance(self):
+        print(f'Your present balance is: P {self.accountBalance}')
+        self.transactions.append(('check-balance', self.accountBalance))
+
+    # Display list of transactions
+    def printTransactions(self, operations):
+        # transform all transactions to print lines as formatted string
+        lines = map(lambda trans: f'{trans[0]} = {trans[1]}', operations)
+        counter = 1
+        for line in lines:
+            print(f'{counter}: {line}')
+            counter += 1
+
+    # Display all transactions
+    def printSummary(self):
+        self.printTransactions(self.transactions)
+
+    # Display only withdrawal transactions
+    def printWithdrawSummary(self):
+        withdraws = filter(
+            lambda trans: trans[0] == 'withdraw' , 
+            self.transactions 
+        )
+        self.printTransactions(withdraws)
 
 # print menu to console providing options
 def printMenu():
@@ -21,68 +61,24 @@ def printMenu():
     print(f"\tEnter {WITHDRAW_SUMMARY}: summary of withdraws")
     print(f"\tEnter {TERMINATE}: exit")
 
-# manage withdrawal operation
-def withdrawAmount():
-    # accountBalance as global allowing modification/mutate
-    global accountBalance
-    withdraw = float(input('Enter amount to withdraw:'))
-    if withdraw > accountBalance:
-        print('Invalid amount')
-    else:
-        accountBalance -= withdraw
-        transactions.append(('withdraw', withdraw))
-
-# manage deposit operation
-def depositAmount():
-    # accountBalance as global allowing modification/mutate
-    global accountBalance
-    deposit = float(input('Enter amount to deposit:'))
-    if deposit <= 0:
-        print('Invalid amount')
-    else:
-        accountBalance += deposit
-        transactions.append(('deposit', deposit))
-
-# Display present balance
-def checkBalance():
-    print(f'Your present balance is: P {accountBalance}')
-    transactions.append(('check-balance', accountBalance))
-
-# Display list of transactions
-def printTransactions(operations):
-    # transform all transactions to print lines as formatted string
-    lines = map(lambda trans: f'{trans[0]} = {trans[1]}', operations)
-    counter = 1
-    for line in lines:
-        print(f'{counter}: {line}')
-        counter += 1
-
-# Display all transactions
-def printSummary():
-    printTransactions(transactions)
-
-# Display only withdrawal transactions
-def printWithdrawSummary():
-    withdraws = filter(lambda trans: trans[0] == 'withdraw' , transactions )
-    printTransactions(withdraws)
-
 # entry point
 def main():
     # initialize operation not(TERMINATE) allowing initial loop entry
+    account = BankAccount("Luffy")
     operation = BALANCE
     while operation != TERMINATE:
         printMenu()
         operation = int(input("Enter op code: "))
         if operation == WITHDRAW:
-            withdrawAmount()
+            account.withdrawAmount()
         elif operation == DEPOSIT:
-            depositAmount()
+            account.depositAmount()
         elif operation == BALANCE:
-            checkBalance()
+            account.checkBalance()
         elif operation == SUMMARY:
-            printSummary()
+            account.printSummary()
         elif operation == WITHDRAW_SUMMARY:
-            printWithdrawSummary()
+            account.printWithdrawSummary()
         elif operation == TERMINATE:
             print('Good bye')
         else:
